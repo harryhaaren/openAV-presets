d060c1143b9cef91e95639334283665367056cdb
Harry van Haaren <harryhaaren@gmail.com>
2013-05-19 16:05:39 +0100

    -Added install script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo -e "\e[34m:: \e[97mCopying GUI to $HOME/.lv2/..."
+install -d $HOME/.lv2/
+cp -r ./* ~/.lv2/
+rm ~/.lv2/install.sh
+echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]\e[97m"
