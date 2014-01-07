01e6745ad6066dbaa6c7b87a94938b433d20f844
Harry van Haaren <harryhaaren@gmail.com>
2014-01-07 14:45:12 +0000

    -Fixed durtySound preset

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ echo -e "\e[34m:: \e[97mCopying GUI to $HOME/.lv2/..."
 install -d $HOME/.lv2/
 cp -r ./* ~/.lv2/
 rm ~/.lv2/install.sh
+rm ~/.lv2/README
 echo -e "\e[1A\e[73C \e[34m[\e[97mDONE\e[34m]\e[97m"
